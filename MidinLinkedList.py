ac93c8f80cb4afecc5ef5de3d07c7deef6eb0da1
Tirumala-Sai-Teja <80871045+Tirumala-Sai-Teja@users.noreply.github.com>
2021-07-01 23:59:12 +0530

    Update MidinLinkedList.py

--- MidinLinkedList.py
+++ MidinLinkedList.py
@@ -9,3 +9,16 @@ def findMid(head):
         n=n.next
  
     return c[len(c)//2]
+def findMid(head):
+    if head is None:
+        return None
+    slow=head
+    fast=head
+    
+    while(fast is not None and fast.next is not None):
+        slow=slow.next # this pointer moves 1 nodes ahead everytime loop is run
+        fast=fast.next.next
+
+        # this pointer moves 2 nodes ahead everytime loop is run
+        
+    return slow.data
